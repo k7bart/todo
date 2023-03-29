ffff751f1299c7f0285a2bce82e55c3f57b21724
Kateryna Bartienieva <k7bart@gmail.com>
2023-03-29 18:14:24 +0300

    refactor

--- script.js
+++ script.js
@@ -1,96 +1,85 @@
-let texts = document.getElementsByClassName("text");
-let checkmarks = document.querySelectorAll(".checkmark");
+let taskList = document.getElementById("task-list");
 
-for (let i = 0; i < checkmarks.length; i++) {
-    checkmarks[i].addEventListener("click", function () {
-        if (texts[i].style["text-decoration"] === "line-through") {
-            texts[i].style["text-decoration"] = "none";
-        } else {
-            texts[i].style["text-decoration"] = "line-through";
+function createNewTask(str) {
+    let newDiv = document.createElement("div");
+    newDiv.className = "task";
+
+    let newLabel = document.createElement("label");
+    newLabel.className = "wraper";
+
+    let newSpan = document.createElement("span");
+    newSpan.className = "checkmark";
+
+    let newInput = document.createElement("input");
+    newInput.setAttribute("type", "checkbox");
+
+    let newP = document.createElement("p");
+    newP.className = "text";
+    newP.innerText = str;
+
+    let newButton = document.createElement("button");
+    newButton.innerText = "—";
+    newButton.className = "remove";
+
+    newLabel.appendChild(newInput);
+    newLabel.appendChild(newSpan);
+    newLabel.appendChild(newP);
+    newLabel.appendChild(newButton);
+
+    newDiv.appendChild(newLabel);
+
+    taskList.appendChild(newDiv);
+
+    newLabel.addEventListener("click", function (event) {
+        if (event.target === newInput) {
+            // handle checkbox click
+            if (newInput.checked) {
+                newP.style.textDecoration = "line-through";
+            } else {
+                newP.style.textDecoration = "none";
+            }
+        } else if (event.target === newButton) {
+            // handle button click
+            newDiv.remove();
         }
     });
 }
 
-let taskList = document.getElementById("task-list");
-let removeButton = document.getElementsByClassName("remove");
-
-for (let i = 0; i < removeButton.length; i++) {
-    removeButton[i].addEventListener("click", function () {
-        taskList.removeChild(taskList.childNodes[i]);
-    });
-}
+createNewTask("wash the dishes");
+createNewTask("walk the dog");
+createNewTask("water plants");
+createNewTask("bake bread");
 
 let newTaskInput = document.getElementById("new-task-input");
 
-newTaskInput.addEventListener("keydown", function (event) {
+newTask = newTaskInput.addEventListener("keydown", function (event) {
     if (event.key !== "Enter") return;
 
     event.preventDefault();
 
-    taskList.innerHTML += `
-        <div class="task">
-            <label class="wraper">
-                <input type="checkbox" />
-                <span class="checkmark"></span>
-                <p class="text">${event.target.value}</p>
-                <button class="remove">—</button>
-            </label>
-        </div>
-    `;
-
-    event.target.value = "";
-});
-
-/*
-newTaskInput.addEventListener("keydown", function (event) {
-    if (event.key !== "Enter") return;
-
-    event.preventDefault();
-
-    // const newTaskText = newTaskInput.value;
-
-    // let newDiv = document.createElement("div");
-    // newDiv.className = "task";
-    // newDiv.id = "task";
-
-    taskList.innerHTML += `
-        <div class="task">
-            <label class="wraper">
-                <input type="checkbox" />
-                <span class="checkmark"></span>
-                <p class="text">${event.target.value}</p>
-                <button class="remove">—</button>
-            </label>
-        </div>
-    `;
+    const newTaskText = newTaskInput.value;
 
     // clear the input after submit
     event.target.value = "";
 
-    // let newLabel = document.createElement("label");
-    // newLabel.className = "wraper";
-
-    // let newSpan = document.createElement("span");
-    // newSpan.className = "checkmark";
-
-    // let newInput = document.createElement("input");
-    // newInput.setAttribute("type", "checkbox");
-
-    // let newP = document.createElement("p");
-    // newP.className = "text";
-    // newP.innerText = newTaskText;
-
-    // let newButton = document.createElement("button");
-    // newButton.innerText = "—";
-    // newButton.className = "remove";
-
-    // newLabel.appendChild(newInput);
-    // newLabel.appendChild(newSpan);
-    // newLabel.appendChild(newP);
-    // newLabel.appendChild(newButton);
-
-    // newDiv.appendChild(newLabel);
-
-    // taskList.appendChild(newDiv);
+    createNewTask(newTaskText);
 });
-*/
+
+// newTaskInput.addEventListener("keydown", function (event) {
+//     if (event.key !== "Enter") return;
+
+//     event.preventDefault();
+
+//     taskList.innerHTML += `
+//         <div class="task">
+//             <label class="wraper">
+//                 <input type="checkbox" />
+//                 <span class="checkmark"></span>
+//                 <p class="text">${event.target.value}</p>
+//                 <button class="remove">—</button>
+//             </label>
+//         </div>
+//     `;
+
+//     event.target.value = "";
+// });
