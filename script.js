274cbe2efceb1b8f017d68ac008275973083e5b5
Kateryna Bartienieva <k7bart@gmail.com>
2023-04-01 11:15:10 +0300

    new images, separated event listeners, started work on reminders

--- script.js
+++ script.js
@@ -1,127 +1,120 @@
 let taskList = document.getElementById("task-list");
-
 function createNewTask(str) {
     let newDiv = document.createElement("div");
-    newDiv.className = "task";
-
+    newDiv.className = "task-wraper";
     let newLabel = document.createElement("label");
-    newLabel.className = "wraper";
-
+    newLabel.className = "task";
     let newSpan = document.createElement("span");
     newSpan.className = "checkmark";
-
     let newInput = document.createElement("input");
     newInput.setAttribute("type", "checkbox");
-
     let newP = document.createElement("p");
     newP.className = "text";
     newP.innerText = str;
-
     let newButton = document.createElement("button");
-    newButton.innerText = "—";
-    newButton.className = "remove";
-
+    newButton.innerHTML = `<svg stroke="currentColor" fill="none" stroke-width="1.3" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="20px" width="20px" ><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>`;
+    newButton.className = "remove-button";
     newLabel.appendChild(newInput);
     newLabel.appendChild(newSpan);
     newLabel.appendChild(newP);
     newLabel.appendChild(newButton);
-
     newDiv.appendChild(newLabel);
-
     taskList.appendChild(newDiv);
 }
 
-function addEventListenersToTasks() {
-    const tasks = taskList.querySelectorAll(".task");
+function addEventListenerToRemoveButton() {
+    const tasks = taskList.querySelectorAll(".task-wraper");
+    tasks.forEach(function (task) {
+        const label = task.querySelector("label");
+        const removeButton = label.querySelector("button");
+        removeButton.addEventListener("click", function () {
+            task.remove();
+            saveTaskListToLocalStorage();
+        });
+    });
+}
+
+function addEventListenerToCheckbox() {
+    const tasks = taskList.querySelectorAll(".task-wraper");
     tasks.forEach(function (task) {
         const label = task.querySelector("label");
         const input = label.querySelector("input");
         const p = label.querySelector("p");
-        const button = label.querySelector("button");
-
-        label.addEventListener("click", function (event) {
-            if (event.target === input) {
-                // handle checkbox click
-                if (input.checked) {
-                    p.style.textDecoration = "line-through";
-                } else {
-                    p.style.textDecoration = "none";
-                }
-            } else if (event.target === button) {
-                // handle button click
-                task.remove();
-                saveTaskListToLocalStorage();
+        input.addEventListener("click", function () {
+            if (input.checked) {
+                p.style.textDecoration = "line-through";
+            } else {
+                p.style.textDecoration = "none";
             }
         });
     });
 }
 
-// how to rewrite the code with using classList.toggle?
-
+// switching theme
 const themeButton = document.getElementById("theme");
-
 isLightTheme = true;
-
 themeButton.addEventListener("click", function () {
     const body = document.querySelector("body");
-    const container = document.getElementById("container");
-    const newTaskInput = document.getElementById("new-task-input");
-
     if (isLightTheme) {
+        body.classList.remove("light-theme");
         body.classList.add("dark-theme");
-        themeButton.innerText = "🌞";
+        themeButton.innerHTML = `<img src="./images/sun.svg" id="sun-svg" version="1.0"  viewBox="0 0 1220.000000 1280.000000"/>`;
         isLightTheme = false;
     } else {
         body.classList.remove("dark-theme");
-        themeButton.innerText = "🌜";
+        body.classList.add("light-theme");
+        themeButton.innerHTML = `<img src="./images/moon.png"
+        hight = 20px
+        width = 20px
+    />`;
         isLightTheme = true;
     }
 });
 
+// adding new task
 let newTaskInput = document.getElementById("new-task-input");
-
 newTask = newTaskInput.addEventListener("keydown", function (event) {
     if (event.key !== "Enter") return;
-
     event.preventDefault();
-
     const newTaskText = newTaskInput.value;
-
     // clear the input after submit
     event.target.value = "";
-
     createNewTask(newTaskText);
-
     saveTaskListToLocalStorage();
 });
 
 function saveTaskListToLocalStorage() {
     localStorage.setItem("taskList", taskList.innerHTML);
-    addEventListenersToTasks();
+    // addEventListenersToTasks();
+    addEventListenerToRemoveButton();
+    addEventListenerToCheckbox();
 }
-
 // Load the task list from local storage if available
 const savedTaskList = localStorage.getItem("taskList");
 if (savedTaskList) {
     taskList.innerHTML = savedTaskList;
 }
 
-addEventListenersToTasks();
+currentMonth = parseInt(date.slice(3, 5)) - 1;
 
-/*
-newTaskInput.addEventListener("keydown", function (event) {
-    if (event.key !== "Enter") return;
-    event.preventDefault();
-    taskList.innerHTML += `
-        <div class="task">
-            <label class="wraper">
-                <input type="checkbox" />
-                <span class="checkmark"></span>
-                <p class="text">${event.target.value}</p>
-                <button class="remove">—</button>
-            </label>
-        </div>
-    `;
-    event.target.value = "";
-});
-*/
+function addTaskForTheFirstDayOfMonth(task) {
+    firstDayOfMonth = new Date(2023, currentMonth, 1).toLocaleDateString();
+    if (date === firstDayOfMonth) {
+        createNewTask(task);
+    }
+}
+
+function addTaskForTheLastDayOfMonth(task) {
+    lastDayOfMonth = new Date(2023, currentMonth, 0).toLocaleDateString();
+    if (date === lastDayOfMonth) {
+        createNewTask(task);
+    }
+}
+
+addTaskForTheFirstDayOfMonth("give pills for the dog");
+addTaskForTheFirstDayOfMonth("change water filter");
+addTaskForTheLastDayOfMonth("transfer counter readings");
+
+// addEventListenersToTasks();
+addEventListenerToRemoveButton();
+addEventListenerToCheckbox();
